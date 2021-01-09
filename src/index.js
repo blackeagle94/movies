1a8c5487f392d67a16383035c1c52a289cca0026
blackeagle94 <blackeagle4894@gmail.com>
2021-01-09 14:41:41 +0200

    wrapped applymiddleware to composewithdevtools

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import './index.css';
 import {createStore, applyMiddleware} from 'redux'
 import rootReducer from './reducers/rootReducer'
 import thunk from 'redux-thunk'
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(<App />, document.getElementById('root'));
