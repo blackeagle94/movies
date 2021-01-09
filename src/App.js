27a751946aa3af962719c6ff41ea4d72e953ca1a
blackeagle94 <blackeagle4894@gmail.com>
2021-01-09 15:25:14 +0200

    created MoviesPage components and added Route to App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react'
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom';
+import MoviesPage from './components/pages/MoviesPage'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        Component App 
-        <br />
-        <Link to='movies'>Movies</Link>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className="App">
+				Component App
+				<br />
+				<Link to="movies">Movies</Link>
+        <Route path='/movies' component={MoviesPage}/>
+			</div>
+		);
+	}
 }
 
-export default App
+export default App;
