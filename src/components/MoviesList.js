4c651effb6778885e7989f7cd9497fa082a35a01
blackeagle94 <blackeagle4894@gmail.com>
2021-01-09 15:59:27 +0200

    created MoviesList.js

---
+++ src/components/MoviesList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import PropTypes from "prop-types";
+
+class MoviesList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+MoviesList.propsTypes = {}
+
+export default MoviesList
