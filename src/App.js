04931942765304f749592c6f7f7413d185eff8cc
blackeagle94 <blackeagle4894@gmail.com>
2021-01-09 18:01:24 +0200

    deleten unnecessary items from App.js and added styleHelper.js

--- src/App.js
+++ src/App.js
@@ -4,90 +4,15 @@ import { Link, Route } from 'react-router-dom';
 import MoviesPage from './components/pages/MoviesPage';
 import _ from 'lodash';
 
-import {
-	Container,
-	Divider,
-	Dropdown,
-	Grid,
-	Header,
-	Icon,
-	Image,
-	List,
-	Menu,
-	Segment,
-	Visibility
+import { Container, Divider, Dropdown, Grid, Header, Icon, Image, List, Menu, Segment, Visibility
 } from 'semantic-ui-react';
+import {menuStyle,
+  fixedMenuStyle,
+  overlayStyle,
+  fixedOverlayStyle,
+  overlayMenuStyle,
+  fixedOverlayMenuStyle} from './components/helpers/styleHelper'
 
-const menuStyle = {
-	border: 'none',
-	borderRadius: 0,
-	boxShadow: 'none',
-	marginBottom: '1em',
-	marginTop: '4em',
-	transition: 'box-shadow 0.5s ease, padding 0.5s ease'
-};
-
-const fixedMenuStyle = {
-	backgroundColor: '#fff',
-	border: '1px solid #ddd',
-	boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.2)'
-};
-
-const overlayStyle = {
-	float: 'left',
-	margin: '0em 3em 1em 0em'
-};
-
-const fixedOverlayStyle = {
-	...overlayStyle,
-	position: 'fixed',
-	top: '80px',
-	zIndex: 10
-};
-
-const overlayMenuStyle = {
-	position: 'relative',
-	left: 0,
-	transition: 'left 0.5s ease'
-};
-
-const fixedOverlayMenuStyle = {
-	...overlayMenuStyle,
-	left: '800px'
-};
-
-const LeftImage = () => (
-	<Image
-		floated="left"
-		size="medium"
-		src="/images/wireframe/square-image.png"
-		style={{ margin: '2em 2em 2em -4em' }}
-	/>
-);
-
-const RightImage = () => (
-	<Image
-		floated="right"
-		size="medium"
-		src="/images/wireframe/square-image.png"
-		style={{ margin: '2em -4em 2em 2em' }}
-	/>
-);
-
-const Paragraph = () => (
-	<p>
-		{[
-			'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum ',
-			'tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas ',
-			'semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ',
-			'ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean ',
-			'fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. ',
-			'Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor ',
-			'neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, ',
-			'accumsan porttitor, facilisis luctus, metus'
-		].join('')}
-	</p>
-);
 
 class App extends Component {
 
@@ -127,13 +52,7 @@ class App extends Component {
         `}
         </style>
 
-        <Container text style={{ marginTop: '2em' }}>
-          <Header as='h1'>Sticky Example</Header>
-          <p>
-            This example shows how to use lazy loaded images, a sticky menu, and a simple text
-            container
-          </p>
-        </Container>
+    
 
         {/* Attaching the top menu is a simple operation, we only switch `fixed` prop and add another style if it has
             gone beyond the scope of visibility
@@ -150,95 +69,20 @@ class App extends Component {
           >
             <Container text>
               <Menu.Item>
-                <Image size='mini' src='/logo.png' />
+                <Image size='mini' src='https://react.semantic-ui.com/logo.png' />
               </Menu.Item>
-              <Menu.Item header>Project Name</Menu.Item>
-              <Menu.Item as='a'>Blog</Menu.Item>
-              <Menu.Item as='a'>Articles</Menu.Item>
-
-              <Menu.Menu position='right'>
-                <Dropdown text='Dropdown' pointing className='link item'>
-                  <Dropdown.Menu>
-                    <Dropdown.Item>List Item</Dropdown.Item>
-                    <Dropdown.Item>List Item</Dropdown.Item>
-                    <Dropdown.Divider />
-                    <Dropdown.Header>Header Item</Dropdown.Header>
-                    <Dropdown.Item>
-                      <i className='dropdown icon' />
-                      <span className='text'>Submenu</span>
-                      <Dropdown.Menu>
-                        <Dropdown.Item>List Item</Dropdown.Item>
-                        <Dropdown.Item>List Item</Dropdown.Item>
-                      </Dropdown.Menu>
-                    </Dropdown.Item>
-                    <Dropdown.Item>List Item</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </Menu.Menu>
+              <Menu.Item header>Movies App</Menu.Item>
+              <Menu.Item as='movies'>Movies</Menu.Item>
+              <Menu.Item >Add New</Menu.Item>
             </Container>
           </Menu>
         </Visibility>
 
         <Container text>
-          {_.times(3, (i) => (
-            <Paragraph key={i} />
-          ))}
+     
+       
 
-          {/* Example with overlay menu is more complex, SUI simply clones all elements inside, but we should use a
-              different approach.
-              An empty Visibility element controls the need to change the fixing of element below, it also uses height
-              and width params received from its ref for correct display.
-            */}
-          <Visibility
-            offset={80}
-            once={false}
-            onTopPassed={this.stickOverlay}
-            onTopVisible={this.unStickOverlay}
-            style={overlayFixed ? { ...overlayStyle, ...overlayRect } : {}}
-          />
-
-          <div ref={this.handleOverlayRef} style={overlayFixed ? fixedOverlayStyle : overlayStyle}>
-            <Menu
-              icon='labeled'
-              style={overlayFixed ? fixedOverlayMenuStyle : overlayMenuStyle}
-              vertical
-            >
-              <Menu.Item>
-                <Icon name='twitter' />
-                Twitter
-              </Menu.Item>
-
-              <Menu.Item>
-                <Icon name='facebook' />
-                Share
-              </Menu.Item>
-
-              <Menu.Item>
-                <Icon name='mail' />
-                Email
-              </Menu.Item>
-            </Menu>
-          </div>
-
-          {_.times(3, (i) => (
-            <Paragraph key={i} />
-          ))}
-          <LeftImage />
-
-          <Paragraph />
-          <RightImage />
-
-          {_.times(4, (i) => (
-            <Paragraph key={i} />
-          ))}
-          <LeftImage />
-
-          <Paragraph />
-          <RightImage />
-
-          {_.times(2, (i) => (
-            <Paragraph key={i} />
-          ))}
+        
         </Container>
 
         <Segment inverted style={{ margin: '5em 0em 0em', padding: '5em 0em' }} vertical>
@@ -282,7 +126,7 @@ class App extends Component {
               </Grid.Row>
             </Grid>
             <Divider inverted section />
-            <Image src='/logo.png' centered size='mini' />
+            <Image src='/https://react.semantic-ui.com/logo.png' centered size='mini' />
             <List horizontal inverted divided link size='small'>
               <List.Item as='a' href='#'>
                 Site Map
