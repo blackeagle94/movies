66c1cfa9810568d01a6a9fb3a3da4be33719b283
blackeagle94 <blackeagle4894@gmail.com>
2021-01-09 18:06:17 +0200

    removed unnecessary items

--- src/App.js
+++ src/App.js
@@ -4,147 +4,106 @@ import { Link, Route } from 'react-router-dom';
 import MoviesPage from './components/pages/MoviesPage';
 import _ from 'lodash';
 
-import { Container, Divider, Dropdown, Grid, Header, Icon, Image, List, Menu, Segment, Visibility
-} from 'semantic-ui-react';
-import {menuStyle,
-  fixedMenuStyle,
-  overlayStyle,
-  fixedOverlayStyle,
-  overlayMenuStyle,
-  fixedOverlayMenuStyle} from './components/helpers/styleHelper'
-
+import { Container, Divider, Grid, Header, Image, List, Menu, Segment, Visibility } from 'semantic-ui-react';
+import { menuStyle, fixedMenuStyle } from './components/helpers/styleHelper';
 
 class App extends Component {
+	state = {
+		menuFixed: false,
+		overlayFixed: false
+	};
 
-  state = {
-    menuFixed: false,
-    overlayFixed: false,
-  }
+	stickOverlay = () => this.setState({ overlayFixed: true });
 
-  handleOverlayRef = (c) => {
-    const { overlayRect } = this.state
+	stickTopMenu = () => this.setState({ menuFixed: null });
 
-    if (!overlayRect) {
-      this.setState({ overlayRect: _.pick(c.getBoundingClientRect(), 'height', 'width') })
-    }
-  }
+	unStickOverlay = () => this.setState({ overlayFixed: null });
 
-  stickOverlay = () => this.setState({ overlayFixed: true })
-
-  stickTopMenu = () => this.setState({ menuFixed: true })
-
-  unStickOverlay = () => this.setState({ overlayFixed: false })
-
-  unStickTopMenu = () => this.setState({ menuFixed: false })
+	unStickTopMenu = () => this.setState({ menuFixed: false });
 
 	render() {
-    const { menuFixed, overlayFixed, overlayRect } = this.state
+		const { menuFixed, overlayFixed, overlayRect } = this.state;
 		return (
-      <div>
-        {/* Heads up, style below isn't necessary for correct work of example, simply our docs defines other
-            background color.
-          */}
-        <style>
-          {`
-          html, body {
-            background: #fff;
-          }
-        `}
-        </style>
+			<div>
+				<Visibility onBottomPassed={this.stickTopMenu} onBottomVisible={this.unStickTopMenu} once={false}>
+					<Menu
+						borderless
+						fixed={menuFixed ? 'top' : undefined}
+						style={menuFixed ? fixedMenuStyle : menuStyle}
+					>
+						<Container text>
+							<Menu.Item>
+								<Image size="mini" src="https://react.semantic-ui.com/logo.png" />
+							</Menu.Item>
+							<Menu.Item header>Movies App</Menu.Item>
+							<Menu.Item as="movies">Movies</Menu.Item>
+							<Menu.Item>Add New</Menu.Item>
+						</Container>
+					</Menu>
+				</Visibility>
 
-    
+				<Container text />
 
-        {/* Attaching the top menu is a simple operation, we only switch `fixed` prop and add another style if it has
-            gone beyond the scope of visibility
-          */}
-        <Visibility
-          onBottomPassed={this.stickTopMenu}
-          onBottomVisible={this.unStickTopMenu}
-          once={false}
-        >
-          <Menu
-            borderless
-            fixed={menuFixed ? 'top' : undefined}
-            style={menuFixed ? fixedMenuStyle : menuStyle}
-          >
-            <Container text>
-              <Menu.Item>
-                <Image size='mini' src='https://react.semantic-ui.com/logo.png' />
-              </Menu.Item>
-              <Menu.Item header>Movies App</Menu.Item>
-              <Menu.Item as='movies'>Movies</Menu.Item>
-              <Menu.Item >Add New</Menu.Item>
-            </Container>
-          </Menu>
-        </Visibility>
-
-        <Container text>
-     
-       
-
-        
-        </Container>
-
-        <Segment inverted style={{ margin: '5em 0em 0em', padding: '5em 0em' }} vertical>
-          <Container textAlign='center'>
-            <Grid columns={4} divided stackable inverted>
-              <Grid.Row>
-                <Grid.Column>
-                  <Header inverted as='h4' content='Group 1' />
-                  <List link inverted>
-                    <List.Item as='a'>Link One</List.Item>
-                    <List.Item as='a'>Link Two</List.Item>
-                    <List.Item as='a'>Link Three</List.Item>
-                    <List.Item as='a'>Link Four</List.Item>
-                  </List>
-                </Grid.Column>
-                <Grid.Column>
-                  <Header inverted as='h4' content='Group 2' />
-                  <List link inverted>
-                    <List.Item as='a'>Link One</List.Item>
-                    <List.Item as='a'>Link Two</List.Item>
-                    <List.Item as='a'>Link Three</List.Item>
-                    <List.Item as='a'>Link Four</List.Item>
-                  </List>
-                </Grid.Column>
-                <Grid.Column>
-                  <Header inverted as='h4' content='Group 3' />
-                  <List link inverted>
-                    <List.Item as='a'>Link One</List.Item>
-                    <List.Item as='a'>Link Two</List.Item>
-                    <List.Item as='a'>Link Three</List.Item>
-                    <List.Item as='a'>Link Four</List.Item>
-                  </List>
-                </Grid.Column>
-                <Grid.Column>
-                  <Header inverted as='h4' content='Footer Header' />
-                  <p>
-                    Extra space for a call to action inside the footer that could help re-engage
-                    users.
-                  </p>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-            <Divider inverted section />
-            <Image src='/https://react.semantic-ui.com/logo.png' centered size='mini' />
-            <List horizontal inverted divided link size='small'>
-              <List.Item as='a' href='#'>
-                Site Map
-              </List.Item>
-              <List.Item as='a' href='#'>
-                Contact Us
-              </List.Item>
-              <List.Item as='a' href='#'>
-                Terms and Conditions
-              </List.Item>
-              <List.Item as='a' href='#'>
-                Privacy Policy
-              </List.Item>
-            </List>
-          </Container>
-        </Segment>
-      </div>
-    )
+				<Segment inverted style={{ margin: '5em 0em 0em', padding: '5em 0em' }} vertical>
+					<Container textAlign="center">
+						<Grid columns={4} divided stackable inverted>
+							<Grid.Row>
+								<Grid.Column>
+									<Header inverted as="h4" content="Group 1" />
+									<List link inverted>
+										<List.Item as="a">Link One</List.Item>
+										<List.Item as="a">Link Two</List.Item>
+										<List.Item as="a">Link Three</List.Item>
+										<List.Item as="a">Link Four</List.Item>
+									</List>
+								</Grid.Column>
+								<Grid.Column>
+									<Header inverted as="h4" content="Group 2" />
+									<List link inverted>
+										<List.Item as="a">Link One</List.Item>
+										<List.Item as="a">Link Two</List.Item>
+										<List.Item as="a">Link Three</List.Item>
+										<List.Item as="a">Link Four</List.Item>
+									</List>
+								</Grid.Column>
+								<Grid.Column>
+									<Header inverted as="h4" content="Group 3" />
+									<List link inverted>
+										<List.Item as="a">Link One</List.Item>
+										<List.Item as="a">Link Two</List.Item>
+										<List.Item as="a">Link Three</List.Item>
+										<List.Item as="a">Link Four</List.Item>
+									</List>
+								</Grid.Column>
+								<Grid.Column>
+									<Header inverted as="h4" content="Footer Header" />
+									<p>
+										Extra space for a call to action inside the footer that could help re-engage
+										users.
+									</p>
+								</Grid.Column>
+							</Grid.Row>
+						</Grid>
+						<Divider inverted section />
+						<Image src="/https://react.semantic-ui.com/logo.png" centered size="mini" />
+						<List horizontal inverted divided link size="small">
+							<List.Item as="a" href="#">
+								Site Map
+							</List.Item>
+							<List.Item as="a" href="#">
+								Contact Us
+							</List.Item>
+							<List.Item as="a" href="#">
+								Terms and Conditions
+							</List.Item>
+							<List.Item as="a" href="#">
+								Privacy Policy
+							</List.Item>
+						</List>
+					</Container>
+				</Segment>
+			</div>
+		);
 	}
 }
 
