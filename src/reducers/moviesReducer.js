040e30353938e50b257fcbfad280152a6a520e9f
blackeagle94 <blackeagle4894@gmail.com>
2021-01-09 14:30:12 +0200

    added moviesReducer

---
+++ src/reducers/moviesReducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+
+}
+
+ const moviesReducer = (state = [], { type, payload }) => {
+    switch (type) {
+
+    // case typeName:
+    //     return { ...state, ...payload }
+
+    default:
+        return state
+    }
+}
+export default moviesReducer
