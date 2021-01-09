5d4d7c26b1591035de901d4059618f4af60c2adc
blackeagle94 <blackeagle4894@gmail.com>
2021-01-09 14:44:21 +0200

    provider imported from react-redux and wrapped APP comp

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import {createStore, applyMiddleware} from 'redux'
 import rootReducer from './reducers/rootReducer'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from "redux-devtools-extension";
+import { Provider } from "react-redux";
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider>
+    <App />
+</Provider>, document.getElementById('root'));
