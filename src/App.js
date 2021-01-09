27d6b0a940c887e50972d31508d3d0362a2c3bda
blackeagle94 <blackeagle4894@gmail.com>
2021-01-09 15:19:56 +0200

    Link added to app.js to go movies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        Component App 
+        <br />
+        <Link to='movies'>Movies</Link>
       </div>
     )
   }
