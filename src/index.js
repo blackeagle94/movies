4d0419b579840b5feed6844ef8306cc70eda919d
blackeagle94 <blackeagle4894@gmail.com>
2021-01-09 15:18:00 +0200

    App wrapped with Router

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import {createStore, applyMiddleware} from 'redux'
-import rootReducer from './reducers/rootReducer'
-import thunk from 'redux-thunk'
-import { composeWithDevTools } from "redux-devtools-extension";
-import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers/rootReducer';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
-ReactDOM.render(<Provider store={store}>
-    <App />
-</Provider>, document.getElementById('root'));
+ReactDOM.render(
+	<Router>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</Router>,
+	document.getElementById('root')
+);
