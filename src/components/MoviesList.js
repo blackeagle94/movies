d5d23791645f428b70e9de5bd1d61d3e62f75af6
blackeagle94 <blackeagle4894@gmail.com>
2021-01-09 17:08:29 +0200

    added lohic
    added logic that if movies is empty comp returns empty const if not returns movies const

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react'
-import PropTypes from "prop-types";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 const MoviesList = (props) => {
-    
-        return (
-            <div>
-                Movies List Comp
-            </div>
-        )
-    
-}
+
+    const emptyMessage = (<p>There are no movies yet.</p>)
+    const movisesList = (<div>Movies List</div>)
+	return (
+        <div>
+            Movies List Comp
+            {props.movies.length === 0 ? emptyMessage : movisesList}
+        </div>
+    );
+};
 
 MoviesList.propsTypes = {
-    movies: PropTypes.array.isRequired
-}
+	movies: PropTypes.array.isRequired
+};
 
-export default MoviesList
+export default MoviesList;
