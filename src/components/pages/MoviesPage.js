ec2f3ae4088d54c8aae3ad17572bb1feca878959
blackeagle94 <blackeagle4894@gmail.com>
2021-01-09 15:36:18 +0200

    PropsTypes added to control incoming prop types

--- src/components/pages/MoviesPage.js
+++ src/components/pages/MoviesPage.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import PropsTypes from 'prop-types'
 
 class MoviesPage extends Component {
+
+    static propsTypes = {
+        movies: PropsTypes.array.isRequired
+    }
+
     render() {
+        console.log(this.props)
         return (
             <div>
                 <h2>Movies PAge</h2>
