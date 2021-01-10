c0102703b754caffd6ebd7d22bc224bcdb3ad538
blackeagle94 <blackeagle4894@gmail.com>
2021-01-10 14:39:22 +0200

    Format selection applied

--- src/App.js
+++ src/App.js
@@ -1,40 +1,27 @@
-import "./App.css";
-import React, { Component } from "react";
-import {  Route } from "react-router-dom";
-import MoviesPage from "./components/pages/MoviesPage";
+import './App.css';
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import MoviesPage from './components/pages/MoviesPage';
 
-import {
-  Container,
-} from "semantic-ui-react";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-
+import { Container } from 'semantic-ui-react';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 class App extends Component {
+	render() {
+		return (
+			<div>
+				<Header />
 
-  stickOverlay = () => this.setState({ overlayFixed: true });
-
-  stickTopMenu = () => this.setState({ menuFixed: null });
-
-  unStickOverlay = () => this.setState({ overlayFixed: null });
-
-  unStickTopMenu = () => this.setState({ menuFixed: false });
-
-  render() {
-    
-    return (
-      <div>
-		<Header />
-
-        <Container text>
-          Component App
-          <br />
-          <Route path="/movies" component={MoviesPage}></Route>
-        </Container>
-		<Footer />
-      </div>
-    );
-  }
+				<Container text>
+					Component App
+					<br />
+					<Route path="/movies" component={MoviesPage} />
+				</Container>
+				<Footer />
+			</div>
+		);
+	}
 }
 
 export default App;
