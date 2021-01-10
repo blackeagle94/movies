6fb4604e722f99ff5805340e2f761cf877f15a7c
blackeagle94 <blackeagle4894@gmail.com>
2021-01-10 17:39:24 +0200

    movies changed to props

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const MoviesList = (movies) => {
+const MoviesList = (props) => {
 
     const emptyMessage = (<p>There are no movies yet.</p>)
-    const movisesList = (<div>{movies.movies.error.response ? <h3>Error data!</h3> : <p>Hata Yok</p>}</div>)
-    console.log(movies.movies.error)
+    const movisesList = (<div>{props.movies.error.response ? <h3>Error data!</h3> : <p>Hata Yok</p>}</div>)
+    console.log(props.movies.error)
 	return (
         <div>
             Movies List Comp
-            {movies.movies.length === 0 ? emptyMessage : movisesList}
+            {props.movies.length === 0 ? emptyMessage : movisesList}
         </div>
     );
 };
