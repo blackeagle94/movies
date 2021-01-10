341f5d53ff50ba7452c04efa90198655b4bc5534
blackeagle94 <blackeagle4894@gmail.com>
2021-01-10 15:54:54 +0200

    dispatched data to reducer

--- src/actions/moviesAction.js
+++ src/actions/moviesAction.js
@@ -1,13 +1,16 @@
 import axios from 'axios'
 import { API_BASE } from "../config/env";
 
-
+export const FETCH_MOVIES = 'FETCH_MOVIES'
 
 export const fetchMovies = () => {
     return dispatch => {
         axios.get(`${API_BASE}/movies`)
         .then(response => response.data)
-        .then(data => console.log(data))
+        .then(data => dispatch({
+            type: FETCH_MOVIES,
+            payload: data
+        }))
         .catch(error => console.log(error.message))
     }
 }
